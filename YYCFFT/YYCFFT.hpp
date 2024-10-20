cb6efb423b1591a8bc9ee60dad919347dbd8b89b
yyc12345 <yyc12321@outlook.com>
2024-10-20 16:50:00 +0800

    finish new project

--- YYCFFT/YYCFFT.hpp
+++ YYCFFT/YYCFFT.hpp
@@ -1,14 +1,18 @@
 #include <cstdint>
 #include <complex>
+#include <vector>
 #include <cmath>
 #include <numbers>
 #include <memory>
 #include <stdexcept>
+#include <algorithm>
 
 namespace YYCFFT {
 
 	namespace Utils {
 		
+		// NOTE: May be replaced by std::has_single_bit
+
 		template<typename _TyIndex, size_t _N>
 		inline constexpr bool IsPow2() {
 			size_t N = _N;
@@ -17,6 +21,8 @@ namespace YYCFFT {
 			}
 			return N == 1u;
 		}
+		
+		// NOTE: May be replaced by std::bit_floor
 
 		template<typename _TyIndex, size_t _N>
 		inline constexpr _TyIndex GetPow2Exponent() {
@@ -28,7 +34,7 @@ namespace YYCFFT {
 			}
 			return --ret;
 		}
-		
+
 		template<typename _TyIndex, typename _TyFloat, size_t _N>
 		struct ValidateArgs {
 		private:
@@ -41,22 +47,35 @@ namespace YYCFFT {
 
 	}
 
+	enum class WindowType {
+		HanningWindow
+	};
+
 	template<typename _TyIndex, typename _TyFloat, size_t _N, std::enable_if_t<Utils::ValidateArgs<_TyIndex, _TyFloat, _N>::value, int> = 0>
 	class Window {
 	private:
 		constexpr static _TyIndex N = _N;
 
 	public:
-		Window() : m_WindowData(nullptr) {
+		Window(WindowType win_type) : m_WindowType(win_type), m_WindowData(nullptr) {
 			// Pre-compute window data
+			// Allocate window buffer
 			m_WindowData = std::make_unique<_TyFloat[]>(N);
-			for (_TyIndex i = 0u; i < N; ++i) {
-				m_WindowData[i] = static_cast<_TyFloat>(0.5) * 
-					(static_cast<_TyFloat>(1.0) - std::cos(static_cast<_TyFloat>(2.0) * std::numbers::pi_v<_TyFloat> * static_cast<_TyFloat>(i) / static_cast<_TyFloat>(N - 1u)));
+			// Assign window data
+			switch (win_type) {
+				case YYCFFT::WindowType::HanningWindow:
+					for (_TyIndex i = 0u; i < N; ++i) {
+						m_WindowData[i] = static_cast<_TyFloat>(0.5) *
+							(static_cast<_TyFloat>(1.0) - std::cos(static_cast<_TyFloat>(2.0) * std::numbers::pi_v<_TyFloat> *static_cast<_TyFloat>(i) / static_cast<_TyFloat>(N - 1u)));
+					}
+					break;
+				default:
+					throw std::invalid_argument("invalid window function type");
 			}
 		}
 
 	private:
+		WindowType m_WindowType;
 		std::unique_ptr<_TyFloat[]> m_WindowData;
 	public:
 		void ApplyWindow(_TyFloat* data) const {
@@ -66,6 +85,9 @@ namespace YYCFFT {
 			for (_TyIndex i = 0u; i < N; ++i) {
 				data[i] *= m_WindowData[i];
 			}
+		}
+		const _TyFloat* GetWindowData() const {
+			return m_WindowData.get();
 		}
 	};
 
@@ -78,14 +100,19 @@ namespace YYCFFT {
 		constexpr static _TyIndex c_HalfPoint = _N >> 1u;
 
 	public:
-		FFT() : m_WNPCache(nullptr) {
+		FFT() : m_WNPCache(nullptr), m_EasyComputeCache(N, _TyComplex()) {
 			// Generate WNP cache
 			m_WNPCache = std::make_unique<_TyComplex[]>(N);
 			for (_TyIndex P = 0u; P < N; ++P) {
 				_TyFloat angle = static_cast<_TyFloat>(2) * std::numbers::pi_v<_TyFloat> * static_cast<_TyFloat>(P) / static_cast<_TyFloat>(N);
-                // e^(-jx) = cosx - j sinx
-				m_WNPCache[P] = std::polar(static_cast<_TyFloat>(1u), angle);
+				// e^(-jx) = cosx - j sinx
+				m_WNPCache[P] = _TyComplex(
+					std::cos(angle),
+					-std::sin(angle)
+				);
 			}
+
+			m_EasyComputeCache = std::vector<_TyComplex>();
 		}
 
 	private:
@@ -97,7 +124,7 @@ namespace YYCFFT {
 			}
 
 			_TyIndex LH, J, K, B, P;
-			LH = J = N >> 1u;
+			LH = J = c_HalfPoint;
 
 			// Construct butterfly structure
 			for (_TyIndex I = 1u; I <= N - 2u; ++I) {
@@ -114,12 +141,12 @@ namespace YYCFFT {
 			// Calculate butterfly
 			_TyComplex temp, temp2;
 			for (_TyIndex L = 1u; L <= M; ++L) {
-				B = 1u << (L - 1u);
+				B = static_cast<_TyIndex>(1u) << (L - 1u);
 				for (J = 0u; J <= B - 1u; ++J) {
-					P = J * (1u << (M - L));
-					
-                    // Use pre-computed cache instead of real-time computing
-					for (_TyIndex KK = J; KK <= N - 1; KK += (1u << L)) {
+					P = J * (static_cast<_TyIndex>(1u) << (M - L));
+
+					// Use pre-computed cache instead of real-time computing
+					for (_TyIndex KK = J; KK <= N - 1u; KK += (static_cast<_TyIndex>(1u) << L)) {
 						temp2 = (data[KK + B] * this->m_WNPCache[P]);
 						temp = temp2 + data[KK];
 						data[KK + B] = data[KK] - temp2;
@@ -128,6 +155,83 @@ namespace YYCFFT {
 				}
 			}
 		}
+
+	private:
+		mutable std::vector<_TyComplex> m_EasyComputeCache;
+	public:
+		/**
+		 * @brief Get the maximum frequency by given sample rate.
+		 * @param[in] sample_rate 
+		 * The sample rate of input stream.
+		 * Unit is Hz or SPS (sample point per second).
+		 * @return 
+		 * The last data in computed FFT drequency data represented frequency.
+		 * Unit is Hz.
+		*/
+		_TyFloat GetMaxFreq(_TyFloat sample_rate) {
+			// Following sample priniciple
+			return sample_rate / static_cast<_TyFloat>(2.0);
+		}
+		/**
+		 * @brief
+		 * The convenient FFT compute function.
+		 * For 
+		 * @param[in] time_scope
+		 * The length of this data must be N.
+		 * For the time order of data, the first data should be the oldest data and the last data should be the newest data.
+		 * @param[out] freq_scope
+		 * The length of this data must be N / 2.
+		 * The first data is 0Hz and the frequency of last data is decided by sample rate which can be computed by GetMaxFreq function in this class.
+		 * @param[in] window
+		 * The window instance applied to data.
+		*/
+		void EasyCompute(const _TyFloat* time_scope, _TyFloat* freq_scope, const Window<_TyIndex, _TyFloat, _N>& window) const {
+			if (time_scope == nullptr || freq_scope == nullptr) [[unlikely]] {
+				throw std::invalid_argument("nullptr data is not allowed for easy FFT computing.");
+			}
+
+			// First, we copy time scope data into cache with reversed order.
+			// because FFT order the first item should be the latest data.
+			// At the same time we multiple it with window function.
+			std::generate(
+				m_EasyComputeCache.begin(),
+				m_EasyComputeCache.end(),
+				[data = &(time_scope[N]), win_data = window.GetWindowData()]() mutable -> _TyComplex {
+					return _TyComplex(*(data--) * *(win_data++), static_cast<_TyFloat>(0));
+				}
+			);
+
+			// Do FFT compute
+			this->Compute(m_EasyComputeCache.data());
+
+			// Compute amplitude
+			for (_TyIndex i = 0u; i < c_HalfPoint; ++i) {
+				freq_scope[i] = static_cast<_TyFloat>(10.0) * std::log10(std::abs(m_EasyComputeCache[i + c_HalfPoint]));
+			}
+		}
 	};
+
+
+	using FFT4F = FFT<size_t, float, 4u>;
+	using FFT8F = FFT<size_t, float, 8u>;
+	using FFT16F = FFT<size_t, float, 16u>;
+	using FFT32F = FFT<size_t, float, 32u>;
+	using FFT64F = FFT<size_t, float, 64u>;
+	using FFT128F = FFT<size_t, float, 128u>;
+	using FFT256F = FFT<size_t, float, 256u>;
+	using FFT512F = FFT<size_t, float, 512u>;
+	using FFT1024F = FFT<size_t, float, 1024u>;
+	using FFT2048F = FFT<size_t, float, 2048u>;
+	
+	using FFT4 = FFT<size_t, double, 4u>;
+	using FFT8 = FFT<size_t, double, 8u>;
+	using FFT16 = FFT<size_t, double, 16u>;
+	using FFT32 = FFT<size_t, double, 32u>;
+	using FFT64 = FFT<size_t, double, 64u>;
+	using FFT128 = FFT<size_t, double, 128u>;
+	using FFT256 = FFT<size_t, double, 256u>;
+	using FFT512 = FFT<size_t, double, 512u>;
+	using FFT1024 = FFT<size_t, double, 1024u>;
+	using FFT2048 = FFT<size_t, double, 2048u>;
 
 }
--- YYCFFT/YYCFFTTestbench.cpp
+++ YYCFFT/YYCFFTTestbench.cpp
@@ -1,11 +1,115 @@
 #include <YYCFFT.hpp>
+#include <initializer_list>
+#include <format>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <chrono>
 
 namespace YYCFFTTestbench {
+
+	using TIndex = size_t;
+	using TFloat = float;
+	using TComplex = std::complex<TFloat>;
+	template<size_t N>
+	using TFFT = YYCFFT::FFT<TIndex, TFloat, N>;
+
+	static void PrintResult(bool ret) {
+		if (ret) std::cout << "Test Pass" << std::endl;
+		else {
+			std::cout << "Test Failed" << std::endl;
+			std::abort();
+		}
+	}
+	template<size_t N>
+	static bool TestPair(const std::initializer_list<TFloat>& _src, const std::initializer_list<TComplex>& _dst) {
+		// Create FFT instance
+		TFFT<N> fft;
+		// Accept input
+		std::vector<TComplex> src(_src.size());
+		std::generate(src.begin(), src.end(), [data = _src.begin()]() mutable -> TComplex { return TComplex(*data++); });
+		std::vector<TComplex> dst(_dst);
+		// Compute FFT
+		fft.Compute(src.data());
+		// Format result
+		//constexpr TFloat tolerance = std::numeric_limits<TFloat>::epsilon();
+		constexpr TFloat tolerance = 0.0003f;
+		for (TIndex i = 0u; i < src.size(); ++i) {
+			if (std::fabs(src[i].real() - dst[i].real()) >= tolerance) return false;
+			if (std::fabs(src[i].imag() - dst[i].imag()) >= tolerance) return false;
+		}
+		return true;
+	}
+	static void TestAnswer() {
+
+		PrintResult(TestPair<8>(
+			{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f}, 
+			{{+3.6000e+01f, +0.0000e+00f}, {-4.0000e+00f, +9.6569e+00f}, {-4.0000e+00f, +4.0000e+00f}, {-4.0000e+00f, +1.6569e+00f}, {-4.0000e+00f, +0.0000e+00f}, {-4.0000e+00f, -1.6569e+00f}, {-4.0000e+00f, -4.0000e+00f}, {-4.0000e+00f, -9.6569e+00f}}
+		));
+		PrintResult(TestPair<8>(
+			{6.0f, 1.0f, 7.0f, 2.0f, 7.0f, 4.0f, 8.0f, 7.0f}, 
+			{{+4.2000e+01f, +0.0000e+00f}, {+4.1421e-01f, +6.6569e+00f}, {-2.0000e+00f, +4.0000e+00f}, {-2.4142e+00f, +4.6569e+00f}, {+1.4000e+01f, +0.0000e+00f}, {-2.4142e+00f, -4.6569e+00f}, {-2.0000e+00f, -4.0000e+00f}, {+4.1421e-01f, -6.6569e+00f}}
+		));
+		PrintResult(TestPair<4>(
+			{1.0f, 2.0f, 3.0f, 4.0f}, 
+			{{+1.0000e+01f, +0.0000e+00f}, {-2.0000e+00f, +2.0000e+00f}, {-2.0000e+00f, +0.0000e+00f}, {-2.0000e+00f, -2.0000e+00f}}
+		));
+		PrintResult(TestPair<4>(
+			{6.0f, 1.0f, 7.0f ,2.0f}, 
+			{{+1.6000e+01f, +0.0000e+00f}, {-1.0000e+00f, +1.0000e+00f}, {+1.0000e+01f, +0.0000e+00f}, {-1.0000e+00f, -1.0000e+00f}}
+		));
+		PrintResult(TestPair<4>(
+			{4.0f, 4.0f, 4.0f ,4.0f}, 
+			{{+1.6000e+01f, +0.0000e+00f}, {+0.0000e+00f, +0.0000e+00f}, {+0.0000e+00f, +0.0000e+00f}, {+0.0000e+00f, +0.0000e+00f}}
+		));
+		PrintResult(TestPair<16>(
+			{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f}, 
+			{{+1.3600e+02f, +0.0000e+00f}, {-8.0000e+00f, +4.0219e+01f}, {-8.0000e+00f, +1.9314e+01f}, {-8.0000e+00f, +1.1973e+01f}, {-8.0000e+00f, +8.0000e+00f}, {-8.0000e+00f, +5.3454e+00f}, {-8.0000e+00f, +3.3137e+00f}, {-8.0000e+00f, +1.5913e+00f}, {-8.0000e+00f, +0.0000e+00f}, {-8.0000e+00f, -1.5913e+00f}, {-8.0000e+00f, -3.3137e+00f}, {-8.0000e+00f, -5.3454e+00f}, {-8.0000e+00f, -8.0000e+00f}, {-8.0000e+00f, -1.1973e+01f}, {-8.0000e+00f, -1.9314e+01f}, {-8.0000e+00f, -4.0219e+01f}}
+		));
+
+	}
+
+	static void TestSpeed() {
+
+		// Prepare FFT engine
+		constexpr TIndex FFTPoint = 1024u;
+		TFFT<FFTPoint> fft;
+
+		// Prepare random buffer
+		constexpr TIndex RndBufCnt = 10u;
+		std::random_device rnd_device;
+		std::default_random_engine rnd_engine(rnd_device());
+		std::uniform_real_distribution<TFloat> rnd_dist(0.0f, 1.0f);
+		std::vector<std::vector<TComplex>> buffer_collection(RndBufCnt);
+		for (auto& buf : buffer_collection) {
+			buf.resize(FFTPoint);
+			std::generate(buf.begin(), buf.end(), [&rnd_engine, &rnd_dist]() mutable -> TComplex { return TComplex(rnd_dist(rnd_engine)); });
+		}
+
+		// Do benchmark
+		constexpr TIndex TestIterCnt = 1000u;
+		auto start_timestamp = std::chrono::high_resolution_clock::now();
+		for (auto& buf : buffer_collection) {
+			for (TIndex i = 0u; i < TestIterCnt; ++i) {
+				fft.Compute(buf.data());
+			}
+		}
+		auto end_timestamp = std::chrono::high_resolution_clock::now();
+
+		// Output result
+		std::chrono::duration<double, std::ratio<1, 1>> elpased = end_timestamp - start_timestamp;
+		std::cout << "Computing 1024-point float-based FFT " << RndBufCnt * TestIterCnt << " times consume " << elpased.count() << " sec" << std::endl;
+		std::cout << "Average time per FFT operation: " << elpased.count() / ((double)RndBufCnt * TestIterCnt) << " sec" << std::endl;
+		
+	}
 
 }
 
 int main(int argc, char* argv[]) {
 
-	YYCFFT::FFT<size_t, float, 4> test;
+	std::cout << "Start testing answer..." << std::endl;
+	YYCFFTTestbench::TestAnswer();
+	std::cout << "Start testing speed..." << std::endl;
+	YYCFFTTestbench::TestSpeed();
 
 }
